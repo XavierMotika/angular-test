5c97e227498db1fa31e6e7993cede56324793a7c
XavierMotika <X.Motika@gmail.com>
2023-09-15 16:49:18 +0200

    - added routes, detail pages with housing infos and an application form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import routeConfig from './routes';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomeComponent],
-  providers: [],
+  imports: [BrowserModule, HomeComponent, RouterModule],
+  providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
